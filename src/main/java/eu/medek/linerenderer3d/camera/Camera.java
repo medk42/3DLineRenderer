78ce03b1e5286be0b173cea72786957547cc96c8
Jakub Medek <medekjak@gmail.com>
2020-01-25 03:28:43 +0100

    Copy from Processing and refactor - camera

---
+++ src/main/java/eu/medek/linerenderer3d/camera/Camera.java
@@ -0,0 +1,33 @@
+package eu.medek.linerenderer3d.camera;
+
+import eu.medek.linerenderer3d.math.Matrix3D;
+import processing.core.PVector;
+
+public class Camera {
+    private float[] position;
+    private float[] rotation;
+
+    public Camera(float[] position, float[] rotation) {
+        this.position = position;
+        this.rotation = rotation;
+    }
+
+    private float[] getPosition() {
+        return position;
+    }
+    private float[] getRotation() {
+        return rotation;
+    }
+
+    public float[][] calculateToCameraMatrix() {
+        return Matrix3D.multiply(Matrix3D.getRotateX(-rotation[0]), Matrix3D.getRotateY(-rotation[1]), Matrix3D.getRotateZ(-rotation[2]), Matrix3D.getTranslate(-position[0], -position[1], -position[2]));
+    }
+
+    public PVector getTransformedVector(PVector forward) {
+        float[][] rotationMatrix = Matrix3D.multiply(Matrix3D.getRotateZ(rotation[2]), Matrix3D.getRotateY(rotation[1]), Matrix3D.getRotateX(rotation[0]));
+        forward = Matrix3D.toPosition(Matrix3D.multiply(rotationMatrix, Matrix3D.toVector(forward)));
+
+        return forward;
+    }
+
+}
