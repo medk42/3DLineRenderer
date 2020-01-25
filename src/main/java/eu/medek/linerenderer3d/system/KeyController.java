9e591f0f7b0d429447557ae3db11b437f978510e
Jakub Medek <medekjak@gmail.com>
2020-01-25 02:55:33 +0100

    Copy from Processing and refactor - system (KeyController)

---
+++ src/main/java/eu/medek/linerenderer3d/system/KeyController.java
@@ -0,0 +1,32 @@
+package eu.medek.linerenderer3d.system;
+
+class KeyController {
+    private boolean[] pressedKeys = new boolean[256];
+    private boolean[] toggledKeys = new boolean[256];
+    private boolean debugLogging;
+
+    public KeyController(boolean debugLogging) {
+        this.debugLogging = debugLogging;
+    }
+
+    public boolean isPressed(char keyc) {
+        return pressedKeys[keyc];
+    }
+
+    public boolean isToggled(char keyc) {
+        return toggledKeys[keyc];
+    }
+
+    public void keyPressed(char keyc) {
+        if (keyc >= pressedKeys.length) return;
+        pressedKeys[keyc] = true;
+        if (debugLogging) System.out.println(keyc + " pressed");
+    }
+
+    public void keyReleased(char keyc) {
+        if (keyc >= pressedKeys.length) return;
+        pressedKeys[keyc] = false;
+        toggledKeys[keyc] = !toggledKeys[keyc];
+        if (debugLogging) System.out.println(keyc + " released");
+    }
+}
