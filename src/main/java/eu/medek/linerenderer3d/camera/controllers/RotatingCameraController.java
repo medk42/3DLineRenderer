fbd245f8b007642ee528cf456a07dca7366350e6
Jakub Medek <medekjak@gmail.com>
2020-02-08 23:35:36 +0100

    Add documentation to RotatingCameraController class

--- src/main/java/eu/medek/linerenderer3d/camera/controllers/RotatingCameraController.java
+++ src/main/java/eu/medek/linerenderer3d/camera/controllers/RotatingCameraController.java
@@ -3,11 +3,37 @@ package eu.medek.linerenderer3d.camera.controllers;
 import eu.medek.linerenderer3d.camera.Camera;
 import eu.medek.linerenderer3d.system.Vector;
 
+/**
+ * Class for moving a Camera in a circle around certain point.
+ */
 public class RotatingCameraController {
+    /**
+     * Point to rotate the camera around.
+     */
     private Vector center;
-    private float radius, deltaHeight;
+
+    /**
+     * Horizontal distance from the center - radius of the circle.
+     */
+    private float radius;
+
+    /**
+     * Vertical distance from the center.
+     */
+    private float deltaHeight;
+
+    /**
+     * Time to complete one circle.
+     */
     private float period;
 
+    /**
+     * Create the rotating camera controller. Rotates camera around the center looking at the center.
+     * @param center point to rotate the camera around
+     * @param radius horizontal distance from the center - radius of the circle
+     * @param deltaHeight vertical distance from the center
+     * @param period time to complete one circle
+     */
     public RotatingCameraController(Vector center, float radius, float deltaHeight, float period) {
         this.center = new Vector(center);
         this.radius = radius;
@@ -15,6 +41,11 @@ public class RotatingCameraController {
         this.period = period;
     }
 
+    /**
+     * Set camera position and rotation to reflect the requested time.
+     * @param camera camera to set up
+     * @param time time for which to set the camera up
+     */
     public void setupCamera(Camera camera, float time) {
         float angle = (time/period % 1) * (float) Math.PI * 2;
         Vector newPositionDelta2D = Vector.fromAngle(angle).normalize().mult(radius);
